cfe948d4ff34b59777937d9045dfd91c8f756553
Hasan-TechNinja <hasantechninaj@gmail.com>
2025-05-28 00:13:27 +0600

    project setup

---
+++ UK_Test_Exam/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for UK_Test_Exam project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UK_Test_Exam.settings')
+
+application = get_asgi_application()
