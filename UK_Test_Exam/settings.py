628c5864d116b3770592f76c4746130d5ede569b
Hasan-TechNinja <hasantechninja@gmail.com>
2025-07-25 22:57:26 +0600

    hosting

--- UK_Test_Exam/settings.py
+++ UK_Test_Exam/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-j_-$b4ag)nr&-c25t1%qf#g$famv+6d_8inn8ap_367=5cul-(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
